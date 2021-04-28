b9fb3f657460132035854f0595622a9d7164b431
Brian_Izaki <bkizaki@outlook.com>
2021-04-28 14:47:06 -0300

    refactor

--- src/main/java/com/algaworks/cobranca/repository/Cobrancas.java
+++ src/main/java/com/algaworks/cobranca/repository/Cobrancas.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.cobranca.model.Cobranca;
-import com.algaworks.cobranca.repository.filter.CobrancaFilter;
 
 // os tipos do JpaRepository é uma entidade e o tipo de dado que é do ID que tem nela
 public interface Cobrancas extends JpaRepository<Cobranca, Long>{
